ae2fa0942194bbd6a538ba53ec5f94cce936ecbb
Alzurana <t.sikinger@googlemail.com>
2025-01-23 23:39:03 +0100

    Updated job scheduler example scene to properly illustrate job priorities

--- utilities/job_scheduler/job_scheduler_example.gd
+++ utilities/job_scheduler/job_scheduler_example.gd
@@ -4,6 +4,9 @@ extends Node
 
 
 @onready var label_pending_jobs: Label = %LabelPendingJobs
+@onready var label_pending_jobs_high: Label = %LabelPendingJobsHigh
+@onready var label_pending_jobs_normal: Label = %LabelPendingJobsNormal
+@onready var label_pending_jobs_low: Label = %LabelPendingJobsLow
 
 
 var callable: Callable
@@ -11,22 +14,27 @@ var callable: Callable
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
-		print("Adding 10 jobs to the queue")
-		for i in range(5):
+		print("Adding 60 jobs to the queue")
+		for i in range(20):
 			JobScheduler.add_job(_some_sqrt_job)
 			JobScheduler.add_job(_some_data_job, JobScheduler.PRIORITY.LOW)
+			JobScheduler.add_job(_some_int_job, JobScheduler.PRIORITY.HIGH)
 	# update display
-	label_pending_jobs.text = "Pending Jobs: " + str(JobScheduler.get_pending_job_count())
+	label_pending_jobs.text = "Pending jobs: " + str(JobScheduler.get_pending_job_count())
+	label_pending_jobs_high.text = "High priority: " + str(JobScheduler.get_pending_job_count(JobScheduler.PRIORITY.HIGH))
+	label_pending_jobs_normal.text = "Normal priority: " + str(JobScheduler.get_pending_job_count(JobScheduler.PRIORITY.NORMAL))
+	label_pending_jobs_low.text = "Low priority: " + str(JobScheduler.get_pending_job_count(JobScheduler.PRIORITY.LOW))
 
 
-# This is really just meant to crunch some numbers
+# Crunch some floats
 func _some_sqrt_job() -> void:
 	@warning_ignore("unused_variable")
 	var x: float = 0.0;
 	for i in range(1000000):
 		x = sqrt(i)
 
-# This is really just meant to crunch some numbers
+
+# Move some registers
 func _some_data_job() -> void:
 	var x: float = 0.0;
 	var y: float = 0.0;
@@ -35,3 +43,11 @@ func _some_data_job() -> void:
 		x = y
 		y = z
 		z = x
+
+
+# Do some simple int stuff
+func _some_int_job() -> void:
+	@warning_ignore("unused_variable")
+	var a: int = 1
+	for i in range(1000000):
+		a *= 78345
